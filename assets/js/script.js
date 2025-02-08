587636fdf50992364ef339dd6307650bc5397c70
Albert Campano <albert.campano@nextdigital.es>
2025-02-08 12:44:46 +0100

    mas texto

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("page-title").textContent = texts.pageTitle;
     document.getElementById("main-title").textContent = texts.mainTitle;
     document.getElementById("under-construction").textContent = texts.underConstruction;
+    document.getElementById("event-date").textContent = texts.eventDate;
+    document.getElementById("event-location").textContent = texts.eventLocation;
+    document.getElementById("invitation-message").textContent = texts.invitationMessage;
+    document.getElementById("invitation-details").textContent = texts.invitationDetails;
 });
